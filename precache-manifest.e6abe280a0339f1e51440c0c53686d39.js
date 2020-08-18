94f9a3d207b86b63db5f1c7b2830c723b2b9eb46
VIBA-V <avasyuthechnicals@gmail.com>
2020-08-18 09:40:14 +0530

    Updates

---
+++ precache-manifest.e6abe280a0339f1e51440c0c53686d39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d9b72adf6c1f75bec3978f6dd93192",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82236cd05880a4a7df8f",
+    "url": "/static/css/main.1f9b1682.chunk.css"
+  },
+  {
+    "revision": "f338d7d5bd74995dcd49",
+    "url": "/static/js/2.503fb330.chunk.js"
+  },
+  {
+    "revision": "6d8b5623ac68c1b25a9d474b102c8cc2",
+    "url": "/static/js/2.503fb330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82236cd05880a4a7df8f",
+    "url": "/static/js/main.4374a21a.chunk.js"
+  },
+  {
+    "revision": "33a3267b1f40f0783884",
+    "url": "/static/js/runtime-main.7c7be759.js"
+  }
+]);
